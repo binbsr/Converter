55bb462d6fc9f58e3f7a09fd83cd42615b9c15c7
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-30 15:13:21 +0545

    Refactored factory and transformer

--- Transform/ITransformer.cs
+++ Transform/ITransformer.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Converter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Converter.Transformer
+namespace Converter.Transform
 {
     interface ITransformer
     {
         public double Transform(double valueToConvert, string fromUnit, string toUnit);
+        public Unit GetUnit(string name);
+        public DropdownOption[] FetchDropdownUnitOptions();
     }
 }
