a1f6b5e31742648a8c46bf7149d307705dd1175a
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-09-14 15:45:47 +0545

    Added more neplai units

--- Data/LengthData.cs
+++ Data/LengthData.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 
 namespace Converter.Data
 {
@@ -9,32 +7,31 @@ namespace Converter.Data
         // Taking inch as base
         public Dictionary<string, (string NepaliUnicode, string ShortForm, char UnitFilterType, string Details, double RatioToBase)> GetAvailableUnits()
         {
-            bool isNepali = CultureInfo.CurrentCulture.Name == Culture.Nepali;
+            //bool isNepali = CultureInfo.CurrentCulture.Name == Culture.Nepali;
 
-            var nonNepaliUnits = new Dictionary<string, (string, string, char, string, double)>
+            var units = new Dictionary<string, (string, string, char, string, double)>
             {
-                [LengthUnit.KiloMetre] = (isNepali ? "किलोमिटर" : "Kilometre", "km", IData.SI, IData.SIUnit, 39370.07874),
-                [LengthUnit.Metre] = (isNepali ? "मिटर" : "Metre", "m", IData.SI, IData.SIUnit, 39.37007874),
-                [LengthUnit.CentiMetre] = (isNepali ? "सेन्टिमिटर" : "Centimetre", "cm", IData.SI, IData.SIUnit, 0.3937007874015748),
-                [LengthUnit.MilliMetre] = (isNepali ? "मिलिमिटर" : "Millimetre", "mm", IData.SI, IData.SIUnit, 0.03937007874015748),
-                [LengthUnit.MicroMetre] = (isNepali ? "माइक्रोमिटर" : "Micrometre", "µm", IData.SI, IData.SIUnit, 0.00003937007874015748),
-                [LengthUnit.NanoMetre] = (isNepali ? "नेनोमिटर" : "Nanometre", "nm", IData.SI, IData.SIUnit, 0.0000000393700787402),
-                [LengthUnit.Mile] = (isNepali ? "माइल" : "Mile", "mi", IData.Imperial, IData.ImperialAndUSCustomaryLength, 63360),
-                [LengthUnit.Yard] = (isNepali ? "यार्ड" : "Yard", "yd", IData.Imperial, IData.ImperialAndUSCustomaryLength, 36),
-                [LengthUnit.Feet] = (isNepali ? "फिट" : "Feet", "ft", IData.Imperial, IData.ImperialAndUSCustomaryLength, 12),
-                [LengthUnit.Inch] = (isNepali ? "इन्च" : "Inch", "in", IData.Imperial, IData.ImperialAndUSCustomaryLength, 1),
-            };
-
-            return isNepali ? GetNepalUnits().Concat(nonNepaliUnits).ToDictionary(x => x.Key, x => x.Value) : nonNepaliUnits;
-        }
-
-        private Dictionary<string, (string NepaliUnicode, string ShortForm, char UnitFilterType, string Details, double RatioToBase)> GetNepalUnits()
-        {
-            return new Dictionary<string, (string, string, char, string, double)>
-            {
+                [LengthUnit.KiloMetre] = ("Kilometre", "km", IData.SI, IData.SIUnit, 39370.07874),
+                [LengthUnit.Metre] = ("Metre", "m", IData.SI, IData.SIUnit, 39.37007874),
+                [LengthUnit.CentiMetre] = ("Centimetre", "cm", IData.SI, IData.SIUnit, 0.3937007874015748),
+                [LengthUnit.MilliMetre] = ("Millimetre", "mm", IData.SI, IData.SIUnit, 0.03937007874015748),
+                [LengthUnit.MicroMetre] = ("Micrometre", "µm", IData.SI, IData.SIUnit, 0.00003937007874015748),
+                [LengthUnit.NanoMetre] = ("Nanometre", "nm", IData.SI, IData.SIUnit, 0.0000000393700787402),
+                [LengthUnit.Mile] = ("Mile", "mi", IData.Imperial, IData.ImperialAndUSCustomaryLength, 63360),
+                [LengthUnit.Yard] = ("Yard", "yd", IData.Imperial, IData.ImperialAndUSCustomaryLength, 36),
+                [LengthUnit.Feet] = ("Feet", "ft", IData.Imperial, IData.ImperialAndUSCustomaryLength, 12),
+                [LengthUnit.Inch] = ("Inch", "in", IData.Imperial, IData.ImperialAndUSCustomaryLength, 1),
                 [LengthUnit.Angul] = ("अँगुल", "अँगुल", IData.Nepali, IData.NepalIndiaCustomaryLength, 0.75),
-                [LengthUnit.Dhanurgrah] = ("धनुरग्रह", "धनुरग्रह", IData.Nepali, IData.NepalIndiaCustomaryLength, 3)
+                [LengthUnit.Dhanurgrah] = ("धनुरग्रह", "धनुरग्रह", IData.Nepali, IData.NepalIndiaCustomaryLength, 3),
+                [LengthUnit.Dhanurmushti] = ("धनुरमुश्ती", "धनुरमुश्ती", IData.Nepali, IData.NepalIndiaCustomaryLength, 6),
+                [LengthUnit.Bittaa] = ("बित्ता", "बित्ता", IData.Nepali, IData.NepalIndiaCustomaryLength, 9),
+                [LengthUnit.Haath] = ("हात", "हात", IData.Nepali, IData.NepalIndiaCustomaryLength, 18),
+                [LengthUnit.Dhanush] = ("धनुष (Bow)", "धनुष", IData.Nepali, IData.NepalIndiaCustomaryLength, 72),
+                [LengthUnit.Haath] = ("कोश", "कोश", IData.Nepali, IData.NepalIndiaCustomaryLength, 144000),
+                [LengthUnit.Dhanush] = ("योजन", "योजन", IData.Nepali, IData.NepalIndiaCustomaryLength, 576000)
             };
+
+            return units;
         }
 
         public string CurrentNamedInstance => nameof(LengthData);
